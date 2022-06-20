e9272a04b164c6fab016d42c340163b7054a0ca6
ilya120 <5374728@gmail.com>
2022-06-20 10:47:00 +0300

    "feat: добавлен loger запросов и ошибок"

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/errorHandler');
 const routes = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,10 +13,16 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
+// helmet для защиты приложение от некоторых широко известных веб-уязвимостей
 app.use(helmet());
+// логгер запросов
+app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
+
+// логгер ошибок
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
