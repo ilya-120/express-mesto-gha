99588b569fb5e8a51074aa36f767ca5dca1adda8
ilya120 <5374728@gmail.com>
2022-06-28 12:38:46 +0300

    "feat: дополнил для ПР15"

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -5,14 +5,14 @@ const BadRequestError = require('../errors/BadRequestError');
 const signIn = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().max(30),
+    password: Joi.string().required(),
   }),
 });
 
 const signUp = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().max(30),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom((value) => {
